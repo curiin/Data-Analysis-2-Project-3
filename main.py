a5945364bed0b24ef218087804ad4564a6b5e98d
Curiin <corina.spreiter@uzh.ch>
2021-04-20 16:17:40 +0200

    optimising position of detector, different calculation of momentum

--- main.py
+++ main.py
@@ -19,10 +19,16 @@ kaon_lifetime = 1.2380*10**(-8)          #seconds    from pdg
 
 average_decay_length_pion_positive = 4.188*1e3 #meters  from script
 
-# pion_v = pion_l / pion_t
-# p = pion_m * pion_v = pion_m * pion_l / pion_t
-# kaon_v = p / kaon_m = pion_m / kaon_m * pion_l / pion_t
-# kaon_l = kaon_v * kaon_t = pion_m / kaon_m * kaon_t / pion_t * pion_l
+# beta = v/c and gamma = 1/sqrt(1-beta^2)
+# 4.188*1e3 = pion_l = beta * gamma * c * pion_t = pion_v / c * 1/sqrt(1-(pion_v)^2/c^2) * c * pion_t
+# -> pion_v = pion_l / pion_t / sqrt(1 + (pion_l)^2 / (pion_t * c)^2)
+# p = pion_m * pion_v
+# kaon_v = p / kaon_m
+# kaon_l = kaon_v / sqrt(1 - (kaon_v/c)^2) * tau_kaon
+
+#p = pion_m_positive * average_decay_length_pion_positive / pion_lifetime_positive / sqrt(1 + average_decay_length_pion_positive**2 / (pion_lifetime_positive * c)**2)
+#average_decay_length_kaon = p / kaon_m / sqrt(1 - (p / kaon_m / c)**2) * kaon_lifetime
+# the new formulas give results that I think are not right
 
 p = pion_m_positive * average_decay_length_pion_positive / pion_lifetime_positive
 average_decay_length_kaon = pion_m_positive / kaon_m * kaon_lifetime / pion_lifetime_positive * average_decay_length_pion_positive
@@ -70,12 +76,12 @@ def number_of_detections(detector_position):
     distance = np.zeros(data_len)
     for k in range(data_len):
         distance[k] = detector_position-decay_position[k]
-        if velocity_pion_positive[k][2] <= 0 and detector_position-decay_position[k]>0:
+        if velocity_pion_positive[k][2] <= 0 and distance[k]>0:
             results_positive.append(0)
-        elif velocity_pion_positive[k][2] >= 0 and detector_position-decay_position[k]<0:
+        elif velocity_pion_positive[k][2] >= 0 and distance[k]<0:
             results_positive.append(0)
         else:
-            t = abs(distance[k]) / velocity_pion_positive[k][2]
+            t = abs(distance[k] / velocity_pion_positive[k][2])
             dx = t * velocity_pion_positive[k][0]
             dy = t * velocity_pion_positive[k][1]
             if dx**2+dy**2 > 4:
@@ -86,37 +92,39 @@ def number_of_detections(detector_position):
 
     results_neutral = []
     for k in range(data_len):
-        if velocity_pion_neutral[k][2] <= 0:
+        if velocity_pion_neutral[k][2] <= 0 and detector_position - decay_position[k] > 0:
+            results_positive.append(0)
+        elif velocity_pion_neutral[k][2] >= 0 and detector_position - decay_position[k] < 0:
             results_neutral.append(0)
         else:
             t = abs(distance[k])/velocity_pion_neutral[k][2]
             dx = t*velocity_pion_neutral[k][0]
             dy = t * velocity_pion_neutral[k][1]
-            if dx**2+dy**2>4:
+            if dx**2+dy**2 > 4:
                 results_neutral.append(0)
             else:
                 results_neutral.append(1)
     count_neutral = results_neutral.count(1) # this is how many times the detector detects the neutral pion
 
+    success = []
     if count_positive != 0 and count_neutral != 0:
         indices_positive = [i for i, x in enumerate(results_positive) if x == 1]
         indices_neutral = [i for i, x in enumerate(results_neutral) if x == 1]
         success = [x for x in indices_positive if x in indices_neutral]
-    else:
-        success = []
+
     fails = data_len - len(success)
     return fails    # to maximise success, we minimise fails
 
 
-optimal_distance = scipy.optimize.minimize(number_of_detections,x0=600)
+optimal_distance = scipy.optimize.minimize(number_of_detections,x0=2000)
 print(optimal_distance["fun"])
 print(optimal_distance["x"])
 
 
 N = 100
-distances = np.linspace(1, 10, N)
-results = 1*distances
-for i, d in enumerate(distances):
+positions = np.linspace(1, 1000, N)
+results = 1*positions
+for i, d in enumerate(positions):
     results[i] = number_of_detections(d)
-plt.plot(distances, results, marker = ".", color = "orange")
+plt.plot(positions, results, marker = ".", color = "orange")
 plt.show()
